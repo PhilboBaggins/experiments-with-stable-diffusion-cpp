d49a3253c38447aef67c440d3fd0cf7d4ff9c15b
Phil <PhilboBaggins@users.noreply.github.com>
2023-09-20 19:05:56 +1000

    Make convert script take arguements

--- ggml-convert/build-and-run.sh
+++ ggml-convert/build-and-run.sh
@@ -3,11 +3,28 @@
 set -o nounset  # (set -u) No unset variables
 set -o errexit  # (set -e) Exit if any statement returns non-true value
 
+if [ $# -eq 3 ]; then
+    MODEL_DIRECTORY="$1"
+    MODEL_FILE="$2"
+    OUTPUT_TYPE="$3"
+    echo aaa
+elif [ $# -eq 2 ]; then
+    MODEL_DIRECTORY="$1"
+    MODEL_FILE="$2"
+    OUTPUT_TYPE="f16"
+else
+    echo "Usage: $0 MODEL_DIRECTORY MODEL_FILE [OUTPUT_TYPE]" >&2
+    exit 1
+fi
+
 DOCKER_TAG="ggml-convert"
+
+cd "$(dirname -- "$0")"
 
 docker build -t "${DOCKER_TAG}" .
 
 docker run --rm -it \
-    --mount type=bind,source="/f/AI/StableDiffusion,target=/models/" \
+    --mount type=bind,source="$MODEL_DIRECTORY,target=/models/" \
     "${DOCKER_TAG}" \
-    v1-5-pruned-emaonly.ckpt --out_type f16
+    "$MODEL_FILE" \
+    --out_type "$OUTPUT_TYPE"
